e7141b9785725cf2a44a19249a908f416f065145
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-11 18:28:32 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-//your JS code here. If required.
-function capitalizeOnBlur{
-	const inputField document.getElementbyID('fname');
-	inputField.value = inputField.value.toUppercase();	
-}
-    const inputField = document.getElementById('fname');
-    inputField.addEventListener('blur', capitalizeOnBlur);
